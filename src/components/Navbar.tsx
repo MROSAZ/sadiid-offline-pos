7a89323d65060aee19ad108d0021174de12e3fb3
MROSAZ <sammoudilass3ad@gmail.com>
2025-04-28 20:02:42 +0100

    Update Navbar.tsx
    removed search for products from nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,20 +46,6 @@ const Navbar: React.FC = () => {
             </div>
           </div>
 
-          {/* Center section - search */}
-          <div className="hidden md:block flex-1 max-w-md mx-4">
-            <div className="relative">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <Search className="h-5 w-5 text-gray-400" />
-              </div>
-              <input
-                type="text"
-                placeholder="Search products..."
-                className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-sadiid-500 focus:border-sadiid-500 sm:text-sm"
-              />
-            </div>
-          </div>
-
           {/* Right section */}
           <div className="flex items-center">
             {/* Online status indicator */}
